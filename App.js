eaaa0d0abed046ddcf0b6459518def07e965b615
Matt Chapman <matt@mttchpmn.com>
2019-02-23 18:47:00 +1300

    Implement initial node list functionality

--- App.js
+++ App.js
@@ -1,21 +1,106 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  AsyncStorage,
+  Alert,
+  Button,
+  Text,
+  TextInput,
+  View
+} from "react-native";
+import style from "./style";
+import Axios from "axios";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiAddress: null,
+      apiAddressSet: false, // binary switch to make sure we don't send GET request early
+      nodesList: [],
+      selectedNode: {}
+    };
+  }
+
+  componentDidMount() {
+    try {
+      AsyncStorage.getItem("apiAddress").then(apiAddress => {
+        console.log("API address loaded from local storage:", apiAddress);
+        this.setState({ apiAddress });
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    if (this.state.apiAddressSet) {
+      this.getNodes();
+    }
+  }
+
+  getNodes() {
+    console.log("Fetching Nodes from API...");
+    const addr = `http://${this.state.apiAddress}:5000/node/list`;
+    Axios.get(addr).then(res => {
+      this.setState({ nodesList: res.data });
+    });
+  }
+
+  saveApiAddress() {
+    try {
+      AsyncStorage.setItem("apiAddress", this.state.apiAddress).then(res =>
+        console.log("API address saved to local storage")
+      );
+      Alert.alert(
+        "Success!",
+        `API address saved successfully`,
+        [{ text: "OK", onPress: () => console.log("OK Pressed.") }],
+        { cancelable: false }
+      );
+    } catch (err) {
+      Alert.alert(
+        "Error",
+        `Error saving API address`,
+        [{ text: "OK", onPress: () => console.log("OK Pressed.") }],
+        { cancelable: false }
+      );
+    }
+  }
+
+  nodeList() {
+    return (
+      <View>
+        <Text>Nodes</Text>
+        <Text>{JSON.stringify(this.state)}</Text>
+      </View>
+    );
+  }
+
+  apiAddressEntry() {
+    return (
+      <View>
+        <Text>Enter Flux API Address:</Text>
+
+        <TextInput
+          onChangeText={text => this.setState({ apiAddress: text })}
+          placeholder=""
+          value={this.state.apiAddress}
+        />
+
+        <Button
+          title="Submit"
+          onPress={() => {
+            this.setState({ apiAddressSet: true });
+            this.saveApiAddress();
+            this.getNodes();
+          }}
+        />
+      </View>
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={style.flexContainer}>
+        {this.state.apiAddressSet ? this.nodeList() : this.apiAddressEntry()}
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ style.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  flexContainer: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignItems: "center"
+  },
+  scrollContainer: {
+    width: "100%"
+  },
+  scrollContainerContent: {
+    flex: 1,
+    alignItems: "center"
+  },
+  flexRow: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  heading1: {},
+  heading2: {},
+  heading3: {},
+  content: {}
+});
+
+export default styles;
